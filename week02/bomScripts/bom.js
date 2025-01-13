aea8348026fcf4dff0bb5f882dd781a98062bd3c
Sunight00 <ephraim.ewane@gmail.com>
2025-01-13 22:47:30 +0100

    changes

--- week02/bomScripts/bom.js
+++ week02/bomScripts/bom.js
@@ -1,2 +1,7 @@
-const inuput = querySelector("favchap");
-const list = querySelector("list")
+const input = document.querySelector('#favchap');
+const list = document.querySelector('______'); 
+const button = document.querySelector('button');
+
+
+const newList = document.createElement('li');
+const deletebutton = document.createElement("Delete");

f8f326a58fd0eb2a8a8d7e38b8c7a26f15a57d93
Sunight00 <ephraim.ewane@gmail.com>
2025-01-15 19:07:49 +0100

    hard

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,37 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*Footer*/
 const calendar = new Date()
 const year= document.getElementById("currentyear").innerHTML = calendar.getFullYear();
@@ -40,32 +6,61 @@ const day = calendar.getDay();
 const hour = calendar.getHours();
 const minute = calendar.getMinutes();
 const second = calendar.getSeconds();
-
+const winWidth = window.innerWidth;
 document.getElementById("lastModified").innerHTML = (month+1) + "/" + day + "/" + year +" "+ hour + ":" + minute + ":" + second;
 
 
-
-
-
-window.addEventListener("resize", myFunction);
-
-
-
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-      document.getElementById("h1").innerHTML="haapy"
-    } else {
-      document.body.style.backgroundColor = "pink";
-    }
-  }
   
-  // Create a MediaQueryList object
-  var x = window.matchMedia("(max-width: 700px)")
-  
-  // Call listener function at run time
-  myFunction(x);
-  
-  // Attach listener function on state changes
-  x.addEventListener("change", function() {
-    myFunction(x);
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

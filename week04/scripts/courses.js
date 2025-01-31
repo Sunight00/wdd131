0d5a83ab8d6a78710369f4b906382b5aff4e7ee8
Sunight00 <ephraim.ewane@gmail.com>
2025-01-31 21:48:39 +0100

    v

--- week04/scripts/courses.js
+++ week04/scripts/courses.js
@@ -2,3 +2,25 @@ const aCourse = {
     code: "CSE121b",
     name: "Javascript Language",
   };
+
+const aSection ={
+  sectionOne:{ sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+  sectionTwo:{ sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+}
+
+
+function setCourse()
+{
+    let name = aCourse.name;
+    let number=aCourse.code;
+    document.getElementById("courseName").innerHTML=name;
+    document.getElementById("courseCode").innerHTML=number;
+}
+
+function setSection()
+{
+  aSection
+
+}
+
+setCourse();

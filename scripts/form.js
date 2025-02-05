dd5cf1cfa272061f20bf0f36e0ea98eacfc9521e
Sunight00 <ephraim.ewane@gmail.com>
2025-02-05 20:47:37 +0100

    s

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,51 @@
-const products = ["Choose a Product...","Tooth Paste","Tooth Brush","Mouth Wash","CPU"];
+//const products = ["Choose a Product...","Tooth Paste","Tooth Brush","Mouth Wash","CPU"];
+const products = ["Choose a Product..."];
 function createList(){
-    products.forEach(function(product){
-        let item = document.createElement("option");
-        item.innerHTML=product;
-        document.querySelector("#products").appendChild( item.innerHTML=product);
+    products.forEach(product =>
+    {
+        //let list = document.createElement("option");
+
     })
 }
 
 createList();
+let list = document.createElement("option");
+list.innerHTML("happy");
+let l = document.createElement("p");
+l=l.innerHTML="sssssssssssssssssss";
+document.querySelector("#p").appendChild(l);
+
+document.getElementById("k").innerHTML="sss";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*Footer*/
+const calendar = new Date()
+const year= document.getElementById("currentyear").innerHTML = calendar.getFullYear();
+const month = calendar.getMonth();
+const day = calendar.getDay();
+const hour = calendar.getHours();
+const minute = calendar.getMinutes();
+const second = calendar.getSeconds();
+const winWidth = window.innerWidth;
+document.getElementById("lastModified").innerHTML = (month+1) + "/" + day + "/" + year +" "+ hour + ":" + minute + ":" + second;

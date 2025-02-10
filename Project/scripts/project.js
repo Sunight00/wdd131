2732387f955b9cffbe81bbb35ea75ceb23560559
Sunight00 <ephraim.ewane@gmail.com>
2025-02-11 00:42:34 +0100

    vvv

---
+++ Project/scripts/project.js
@@ -0,0 +1,15 @@
+
+const menu = document.querySelector("#menu");
+menu.addEventListener("click",displayMenu());
+
+
+
+
+
+
+function displayMenu()
+{
+    document.getElementById("eg").innerHTML = Date();
+}
+
+

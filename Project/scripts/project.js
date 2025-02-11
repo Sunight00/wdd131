06d64872a1d7e768aff5205e97fe3797ae9a7f12
Sunight00 <ephraim.ewane@gmail.com>
2025-02-11 17:07:27 +0100

    ddd

--- Project/scripts/project.js
+++ Project/scripts/project.js
@@ -32,6 +32,23 @@ function closeMenu()
 
 
 
+const heightOutput = document.querySelector("#height");
+const widthOutput = document.querySelector("#width");
+
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+window.onresize = reportWindowSize;
+
+
+
+
+
+
+
+
 
 
 

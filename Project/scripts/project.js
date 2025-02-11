9942e2668055da73064af8771b69235759a83870
Sunight00 <ephraim.ewane@gmail.com>
2025-02-11 16:57:14 +0100

    we are getting somewhere

--- Project/scripts/project.js
+++ Project/scripts/project.js
@@ -1,15 +1,79 @@
 
 const menu = document.querySelector("#menu");
 menu.addEventListener("click",displayMenu);
+const xmenu = document.querySelector("#xmenu");
+xmenu.addEventListener("click",closeMenu);
 
 
 
 
 
+
+
+let n = document.getElementById("eg");
+let oN = document.querySelector(".bt");
+let oFF = document.querySelector(".btn");
 
 function displayMenu()
 {
-    document.getElementById("eg").innerHTML = Date();
+    n.style.display="block"
+    menu.style.display="none"
+    xmenu.style.display="inline"
+
+}
+
+function closeMenu()
+{
+    
+   
+   n.style.display="none"
+   xmenu.style.display="none"
+   menu.style.display="inline"
+}
+
+function off()
+{
+    
+    n.style.display="none"
+    oN.style.display="block"
+    oFF.style.display="none"
+
 }
 
 
+
+
+let a = 0;
+
+document.querySelector(".btn").addEventListener("click",off)
+document.querySelector(".bt").addEventListener("click",on)
+
+    /*
+    ()=> {
+    if ( a==0) {
+        let n = document.getElementById("eg");
+        n.style.display="display"
+        a=1
+    }
+    else if (a==1)
+    {
+         let n = document.getElementById("eg");
+        n.style.display="none"
+        a=0
+    }
+});
+
+
+
+
+
+
+
+/*var divs = document.querySelectorAll('.list-1 > div'); 
+        var button = document.querySelector('button'); 
+        button.addEventListener("click", () => { 
+            var list_2 = document.querySelector('.list-2'); 
+            divs.forEach((div) => { 
+                list_2.appendChild(div.cloneNode(true)); 
+            }) 
+        }); */

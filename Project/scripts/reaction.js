96882b4f4cfc7f4cdc7d6acf43a076281cbdb4c5
Sunight00 <ephraim.ewane@gmail.com>
2025-02-15 01:03:56 +0100

    xx

--- Project/scripts/reaction.js
+++ Project/scripts/reaction.js
@@ -802,25 +802,44 @@ function remove()
 
 
 
-const top = [
-    {num:"",tsong:"Song",tartist:"Artist",talbum:"Album",ttime:"Time"},
+const to = [
+    {num:"",song:"Song",artist:"Artist",album:"Album",time:"Time"},
     {num:"1",song:"Baby (Is It A Crime)", artist:"Rema", album:"Baby (Is It A Crime) Single", time:"2:44"},
     {num:"2", song:"PITY THIS BOY", artist:"ODUMOBUBLVCK, Victony", album:"PITY THIS BOY- Single", time:"2:45"}
 
 ]
 
-array.forEach(element => {
-    let table= document.createElement("table");
-    let row=document.createElement("tr");
-    let num = document.createElement("td")
-    let song=document.createElement("td");
-    let artist=document.createElement("td");
-    let album=document.createElement("td");
-    let time=document.createElement("td");
 
-    row.appendChild(num);
-    row.appendChild(song);
-    row.appendChild(artist);
-    row.appendChild(album);
-    row.appendChild(time);
-});
+able()
+function able(){
+    to.forEach(element => {
+        let table= document.createElement("table");
+        let row=document.createElement("tr");
+        let num = document.createElement("td")
+        let song=document.createElement("td");
+        let artist=document.createElement("td");
+        let album=document.createElement("td");
+        let time=document.createElement("td");
+       
+
+
+        num.innerHTML=element.num;
+        song.innerHTML=element.song;
+        artist.innerHTML=element.artist;
+        album.innerHTML=element.album;
+        time.innerHTML=element.time;
+
+
+        row.appendChild(num);
+        row.appendChild(song);
+        row.appendChild(artist);
+        row.appendChild(album);
+        row.appendChild(time);
+    
+        table.appendChild(row);
+        let  n = document.querySelector(".chart");
+        n.appendChild(table)
+    });
+}    
+       
+    

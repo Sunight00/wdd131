dff5c6636f7b52197ec5cb169f1a02e8dbd2a3bc
Sunight00 <ephraim.ewane@gmail.com>
2025-02-15 01:26:37 +0100

    ddd

--- Project/scripts/recreate.js
+++ Project/scripts/recreate.js
@@ -1,14 +1,22 @@
 
 const to = [
     {num:"",song:"Song",artist:"Artist",album:"Album",time:"Time"},
-    {num:"1.",song:"Baby (Is It A Crime)", artist:"Rema", album:"Baby (Is It A Crime) Single", time:"2:44"},
-    {num:"2.", song:"PITY THIS BOY", artist:"ODUMOBUBLVCK, Victony", album:"PITY THIS BOY- Single", time:"2:45"}
+    {num:"1. ",song:"Baby (Is It A Crime)", artist:"Rema", album:"Baby (Is It A Crime) Single", time:"2:44"},
+    {num:"2. ", song:"PITY THIS BOY", artist:"ODUMODUBLVCK, Victony", album:"PITY THIS BOY- Single", time:"2:45"},
+    {num:"3",song:"WHY LOVE", artist:"Asake", album:"WHY LOVE - Single", time:"3:30"},
+    {num:"4", song:"MMS",artist:"Askae, Wizkid", album:"Lungu Boy", time:"3:39"},
+    {num:"5", song:"Funds", artist:"Davido feat ODUMODUBLVK & Chike", album:"5ive", time:"3:26"}
+
 
 ]
 
 
 able()
 function able(){
+    let  n = document.querySelector(".chart");
+        let toptitle = document.createElement("h4");
+        toptitle.innerHTML="Top 5 Nigeria Apple Music";
+        n.appendChild(toptitle)
     to.forEach(element => {
         let table= document.createElement("table");
         let row=document.createElement("tr");
@@ -50,7 +58,7 @@ function able(){
         row.appendChild(time);
     
         table.appendChild(row);
-        let  n = document.querySelector(".chart");
+        
         n.appendChild(table)
     });
 }

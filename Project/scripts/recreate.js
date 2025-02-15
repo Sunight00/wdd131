4918a34ecaf3b165d95af16a3b567217980edd00
Sunight00 <ephraim.ewane@gmail.com>
2025-02-15 01:15:28 +0100

    sss

--- Project/scripts/recreate.js
+++ Project/scripts/recreate.js
@@ -1,8 +1,8 @@
 
 const to = [
     {num:"",song:"Song",artist:"Artist",album:"Album",time:"Time"},
-    {num:"1",song:"Baby (Is It A Crime)", artist:"Rema", album:"Baby (Is It A Crime) Single", time:"2:44"},
-    {num:"2", song:"PITY THIS BOY", artist:"ODUMOBUBLVCK, Victony", album:"PITY THIS BOY- Single", time:"2:45"}
+    {num:"1.",song:"Baby (Is It A Crime)", artist:"Rema", album:"Baby (Is It A Crime) Single", time:"2:44"},
+    {num:"2.", song:"PITY THIS BOY", artist:"ODUMOBUBLVCK, Victony", album:"PITY THIS BOY- Single", time:"2:45"}
 
 ]
 
@@ -17,25 +17,36 @@ function able(){
         let artist=document.createElement("td");
         let album=document.createElement("td");
         let time=document.createElement("td");
+        let dash = document.createElement("td")
+        let dash2 = document.createElement("td")
+        let dash3 = document.createElement("td")
        
         num.setAttribute("class","data");
         song.setAttribute("class","data")
         artist.setAttribute("class","data")
         album.setAttribute("class","data")
         time.setAttribute("class","data")
+        
 
 
         num.innerHTML=element.num;
         song.innerHTML=element.song;
+        dash.innerHTML="-----";
         artist.innerHTML=element.artist;
+        dash2.innerHTML="-----";
         album.innerHTML=element.album;
+        dash3.innerHTML="-----";
         time.innerHTML=element.time;
+
 
 
         row.appendChild(num);
         row.appendChild(song);
+        row.appendChild(dash);
         row.appendChild(artist);
+        row.appendChild(dash2);
         row.appendChild(album);
+        row.appendChild(dash3);
         row.appendChild(time);
     
         table.appendChild(row);

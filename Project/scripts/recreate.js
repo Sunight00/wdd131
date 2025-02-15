c27bae6d49c587edcf26995e763d74c38ae0c78f
Sunight00 <ephraim.ewane@gmail.com>
2025-02-15 01:27:00 +0100

    hh

--- Project/scripts/recreate.js
+++ Project/scripts/recreate.js
@@ -3,9 +3,9 @@ const to = [
     {num:"",song:"Song",artist:"Artist",album:"Album",time:"Time"},
     {num:"1. ",song:"Baby (Is It A Crime)", artist:"Rema", album:"Baby (Is It A Crime) Single", time:"2:44"},
     {num:"2. ", song:"PITY THIS BOY", artist:"ODUMODUBLVCK, Victony", album:"PITY THIS BOY- Single", time:"2:45"},
-    {num:"3",song:"WHY LOVE", artist:"Asake", album:"WHY LOVE - Single", time:"3:30"},
-    {num:"4", song:"MMS",artist:"Askae, Wizkid", album:"Lungu Boy", time:"3:39"},
-    {num:"5", song:"Funds", artist:"Davido feat ODUMODUBLVK & Chike", album:"5ive", time:"3:26"}
+    {num:"3.",song:"WHY LOVE", artist:"Asake", album:"WHY LOVE - Single", time:"3:30"},
+    {num:"4.", song:"MMS",artist:"Askae, Wizkid", album:"Lungu Boy", time:"3:39"},
+    {num:"5.", song:"Funds", artist:"Davido feat ODUMODUBLVK & Chike", album:"5ive", time:"3:26"}
 
 
 ]
